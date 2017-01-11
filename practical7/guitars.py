c4bb01e4a265b2ac1c9e9010b06e127d22bce9ae
LKWBrando <brandon.lum@my.jcu.edu.au>
2017-01-11 13:35:04 +0800

    Finished up practical 7,
    Adding new files for practical 8

--- practical7/guitars.py
+++ practical7/guitars.py
@@ -1,19 +1,24 @@
 from practical7.guitar import Guitar
-nameList = []
-yearList = []
-costList = []
 
-checker = 1
-while True:
-        name = input("Name:")
-        if name != "":
-            nameList.append(name)
-            year = input("Year:")
-            yearList.append(year)
-            cost = input("Cost:$")
-            costList.append(cost)
-        else:
-            break
 
-for i in range(len(nameList)):
-    print(Guitar(nameList[i], yearList[i], costList[i]))
+guitar_list = []
+
+count = 0
+while count == 0:
+    input_name = input("Please enter your name.")
+    if input_name == "" or input_name.isspace():
+        count = 1
+    else:
+        input_year = int(input("Input year."))
+        input_cost = int(input("Input cost"))
+        guitar_list.append(Guitar(input_name, input_year, input_cost))
+
+
+for guitar in guitar_list:
+    index = 0
+    guitar_age = guitar.get_age()
+    if guitar.is_vintage(guitar_age) == True:
+        print("Guitar {}: {} ({}), worth ${} ({})".format(index + 1, guitar.name, guitar.year, guitar.cost, "Vintage"))
+    else:
+        print(
+            "Guitar {}: {} ({}), worth ${}".format(index + 1, guitar.name, guitar.year, guitar.cost))
---
+++ practical8/we1.py
@@ -0,0 +1,107 @@
+"""
+CP1404/CP5632 Practical
+Car class
+"""
+import random
+
+class Car:
+    """ represent a car object """
+    def __init__(self, name="", fuel=0):
+        """ initialise a Car instance """
+        self.name = name
+        self.fuel = fuel
+        self.odometer = 0
+        self.price_per_km = 1.20
+
+    def __str__(self):
+        return "{}, fuel={}, odo={}".format(self.name, self.fuel, self.odometer)
+
+    def add_fuel(self, amount):
+        """ add amount to the car's fuel"""
+        self.fuel += amount
+
+    def drive(self, distance):
+        """ drive the car a given distance if it has enough fuel or drive until fuel runs out
+        return the distance actually driven """
+        if distance > self.fuel:
+            distance_driven = self.fuel
+            self.fuel = 0
+        else:
+            self.fuel -= distance
+            distance_driven = distance
+        self.odometer += distance_driven
+        return distance_driven
+
+class Taxi(Car):
+    """ specialised version of a Car that includes fare costs """
+    def __init__(self, name, fuel, price_per_km):
+        """ initialise a Taxi instance, based on parent class Car """
+        super().__init__(name, fuel)
+        Taxi.price_per_km = price_per_km
+        self.current_fare_distance = 0
+
+    def __str__(self):
+        """ return a string representation like a car but with current fare distance"""
+        return "{}, {}km on current fare, ${:.2f}/km".format(super().__str__(), self.current_fare_distance, self.price_per_km)
+
+    def get_fare(self):
+        """ get the price for the taxi trip """
+        return self.price_per_km * self.current_fare_distance
+
+    def start_fare(self):
+        """ begin a new fare """
+        self.current_fare_distance = 0
+
+    def drive(self, distance):
+        """ drive like parent Car but calculate fare distance as well"""
+        distance_driven = super().drive(distance)
+        self.current_fare_distance += distance_driven
+        return distance_driven
+
+class UnreliableCar(Car):
+
+    def __init__(self,name, fuel, reliability):
+        super().__init__(name, fuel)
+        self.reliability = reliability
+
+    def drive(self, distance):
+        rand_num = random.randint(0,100)
+        if rand_num < self.reliability:
+            if distance > self.fuel:
+                distance_driven = self.fuel
+                self.fuel = 0
+            else:
+                self.fuel -= distance
+                distance_driven = distance
+            self.odometer += distance_driven
+            return distance_driven
+        else:
+            print("Do not drive car")
+
+class SilverServiceTaxi(Taxi):
+
+    def __init__(self, name, fuel, distance):
+        super().__init__(self, name, fuel)
+        self.distance = distance
+        self.fanciness = 2
+        self.flagfall = 4.5
+
+    def get_fare(self):
+        return (self.price_per_km * self.distance * self.fanciness) + self.flagfall
+
+prius1 = Taxi("Prius1", 100, 1.2)
+prius1.drive(40)
+print(prius1)
+print("Cost of current fare is: ${}".format(prius1.get_fare()))
+
+prius1.start_fare()
+prius1.drive(100)
+print(prius1)
+print("Cost of current fare is: ${}".format(prius1.get_fare()))
+
+prius2 = UnreliableCar("Prius2", 100, 10)
+prius2.drive(50)
+print(prius2)
+
+prius3 = SilverServiceTaxi("Prius3", 100, 10)
+print(prius3.get_fare())
