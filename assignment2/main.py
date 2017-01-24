c5fc2150e25024345c4ed47944206ce20d7cda19
LKWBrando <brandon.lum@my.jcu.edu.au>
2017-01-24 11:32:43 +0800

    Defining required and completed books to create buttons,
    External classes are ignored at this point for functionality

--- assignment2/main.py
+++ assignment2/main.py
@@ -10,26 +10,66 @@ from assignment2.booklist import BookList
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
+from operator import itemgetter
+
+book_list = []
+req_counter = 1
 
 class ReadingListApp(App):
     def on_start(self): # this will do initialization
-        print("on_start is called.")
         # file opening
-        myfile = open("books.csv", "r")
-        booklists = myfile.readlines()
-        print(booklists)
-        myfile.close()
-        #....linking
-        temp_button = Button(text="Testing one")
-        temp_button.bind(on_release=self.press)
-        self.root.ids.entriesBox.add_widget(temp_button)
+        bookFile = open("books.csv", "r")
+        for line, data in enumerate(bookFile.readlines()):
+            line_index = data.strip()
+            line_data = line_index.split(",")
+            book_list.append(line_data)
+        bookFile.close()
+        book_list.sort(key=itemgetter(1, 2))
+        print("{} books loaded from books.csv".format(len(book_list)))
+        print(book_list)
 
-        temp_button = Button(text="Testing two")
+    def on_required(self):
+        required_page_count = 0
+        required_book_count = 0
+        req_counter = 1
+        for index, book in enumerate(book_list):
+            if book[3] == 'r':
+                required_page_count += int(book[2])
+                required_book_count += 1
+                temp_button = Button(text = ("{:<1}. {:<40s} by {:<20s} {:<4}pages".format(index, book[0], book[1], book[2])))
+                temp_button.bind(on_release=self.press)
+                self.root.ids.entriesBox.add_widget(temp_button)
+        if required_book_count == 0:
+            temp_button = Button(text ="No required books")
+            temp_button.bind(on_release=self.press)
+
+            req_counter = 0
+            return req_counter
+        else:
+            temp_button = Button(text =("Total pages for {} book(s):{}".format(required_book_count, required_page_count)))
+            temp_button.bind(on_release=self.press)
+            self.root.ids.entriesBox.add_widget(temp_button)
+            return req_counter
+
+    def on_completed(self):
+        completed_page_count = 0
+        completed_book_count = 0
+        for index, book in enumerate(book_list):
+            if book[3] == 'c':
+                completed_page_count += int(book[2])
+                completed_book_count += 1
+                temp_button = Button(text=("{:<1}.{:<40s}by {:<20s} {:<4}pages".format(index, book[0], book[1], book[2])))
+                temp_button.bind(on_release=self.press)
+                self.root.ids.entriesBox.add_widget(temp_button)
+        temp_button = Button(text =("Total pages for {} book(s):{}".format(completed_book_count, completed_page_count)))
         temp_button.bind(on_release=self.press)
         self.root.ids.entriesBox.add_widget(temp_button)
 
     def press(self, instance):
         pass
+
+    def clear_all(self):
+        self.root.ids.entriesBox.clear_widgets()
 
     def build(self):
         self.title = "Reading list application"
