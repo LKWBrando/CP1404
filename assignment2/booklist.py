9e9bf528dae19029ac41611744d6be5e2f552849
LKWBrando <brandon.lum@my.jcu.edu.au>
2017-01-24 13:44:05 +0800

    Functionality for listing required and completed books accomplished.
    Funtionality for adding new books added

--- assignment2/booklist.py
+++ assignment2/booklist.py
@@ -1,7 +1,27 @@
+from operator import itemgetter
+
 # create your BookList class in this file
 class BookList:
     def __init__(self):
-        self.booklists = []
+        self.book_list = []
+        bookFile = open("books.csv", "r")
+        for line, data in enumerate(bookFile.readlines()):
+            line_index = data.strip()
+            line_data = line_index.split(",")
+            self.book_list.append(line_data)
+        bookFile.close()
+        self.book_list.sort(key=itemgetter(1, 2))
 
-   # def add_book(self, book):
-    #    self.booklists.append(book)
+    def __getitem__(self, item):
+        return self.book_list[item]
+
+    def add_book(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        line_data = list((self.title, self.author, self.pages, 'r'))
+        self.book_list.append(line_data)
+
+
+#self.booklists.append(book)
+
--- assignment2/main.py
+++ assignment2/main.py
@@ -12,21 +12,10 @@ from kivy.lang import Builder
 from kivy.uix.button import Button
 from operator import itemgetter
 
-book_list = []
+book_list = BookList()
 req_counter = 1
 
 class ReadingListApp(App):
-    def on_start(self): # this will do initialization
-        # file opening
-        bookFile = open("books.csv", "r")
-        for line, data in enumerate(bookFile.readlines()):
-            line_index = data.strip()
-            line_data = line_index.split(",")
-            book_list.append(line_data)
-        bookFile.close()
-        book_list.sort(key=itemgetter(1, 2))
-        print("{} books loaded from books.csv".format(len(book_list)))
-        print(book_list)
 
     def on_required(self):
         required_page_count = 0
@@ -42,14 +31,13 @@ class ReadingListApp(App):
         if required_book_count == 0:
             temp_button = Button(text ="No required books")
             temp_button.bind(on_release=self.press)
-
             req_counter = 0
             return req_counter
         else:
-            temp_button = Button(text =("Total pages for {} book(s):{}".format(required_book_count, required_page_count)))
-            temp_button.bind(on_release=self.press)
-            self.root.ids.entriesBox.add_widget(temp_button)
+            self.root.ids.display_pages.text = ("Total pages for {} book(s):{}".format(required_book_count, required_page_count))
+            self.root.ids.display_text.text = ("Click books to mark them as completed")
             return req_counter
+
 
     def on_completed(self):
         completed_page_count = 0
@@ -61,9 +49,19 @@ class ReadingListApp(App):
                 temp_button = Button(text=("{:<1}.{:<40s}by {:<20s} {:<4}pages".format(index, book[0], book[1], book[2])))
                 temp_button.bind(on_release=self.press)
                 self.root.ids.entriesBox.add_widget(temp_button)
-        temp_button = Button(text =("Total pages for {} book(s):{}".format(completed_book_count, completed_page_count)))
-        temp_button.bind(on_release=self.press)
-        self.root.ids.entriesBox.add_widget(temp_button)
+        self.root.ids.display_pages.text = ("Total pages for {} book(s):{}".format(completed_book_count, completed_page_count))
+        self.root.ids.display_text.text = ("Click on a book for more information")
+
+
+    def save_book(self, input_title, input_author, input_pages):
+        self.input_title = str(input_title)
+        self.input_author = str(input_author)
+        self.input_pages = int(input_pages)
+        book_list.add_book(self.input_title, self.input_author, self.input_pages)
+        self.root.ids.input_title.text = ""
+        self.root.ids.input_author.text = ""
+        self.root.ids.input_pages.text = ""
+        return book_list
 
     def press(self, instance):
         pass
