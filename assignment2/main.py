eda5d60d1a1278af4ec3d08bbcc6113915a0c03c
LKWBrando <brandon.lum@my.jcu.edu.au>
2017-01-24 22:35:47 +0800

    Functionality of addition and removal of temporary widgets completed (I hope)

--- assignment2/main.py
+++ assignment2/main.py
@@ -10,12 +10,17 @@ from assignment2.booklist import BookList
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
+from kivy.properties import StringProperty
 from operator import itemgetter
 
 book_list = BookList()
 req_counter = 1
 
 class ReadingListApp(App):
+    def build(self):
+        self.title = "Reading list application"
+        self.root = Builder.load_file("app.kv")
+        return self.root
 
     def on_required(self):
         required_page_count = 0
@@ -25,8 +30,10 @@ class ReadingListApp(App):
             if book[3] == 'r':
                 required_page_count += int(book[2])
                 required_book_count += 1
-                temp_button = Button(text = ("{:<1}. {:<40s} by {:<20s} {:<4}pages".format(index, book[0], book[1], book[2])))
-                temp_button.bind(on_release=self.press)
+                book_text = book[0]
+                temp_button = Button(text = book_text)
+                temp_button.bind(on_press=lambda x: self.mark(index))
+                temp_button.bind(on_release=lambda x:self.root.ids.entriesBox.remove_widget(temp_button))
                 self.root.ids.entriesBox.add_widget(temp_button)
         if required_book_count == 0:
             temp_button = Button(text ="No required books")
@@ -38,6 +45,10 @@ class ReadingListApp(App):
             self.root.ids.display_text.text = ("Click books to mark them as completed")
             return req_counter
 
+    def mark(self, index):
+        book_list[index].pop(3)
+        book_list[index].insert(3, 'c')
+
 
     def on_completed(self):
         completed_page_count = 0
@@ -46,7 +57,7 @@ class ReadingListApp(App):
             if book[3] == 'c':
                 completed_page_count += int(book[2])
                 completed_book_count += 1
-                temp_button = Button(text=("{:<1}.{:<40s}by {:<20s} {:<4}pages".format(index, book[0], book[1], book[2])))
+                temp_button = Button(text=("{}".format(book[0])))
                 temp_button.bind(on_release=self.press)
                 self.root.ids.entriesBox.add_widget(temp_button)
         self.root.ids.display_pages.text = ("Total pages for {} book(s):{}".format(completed_book_count, completed_page_count))
@@ -58,6 +69,8 @@ class ReadingListApp(App):
         self.input_author = str(input_author)
         self.input_pages = int(input_pages)
         book_list.add_book(self.input_title, self.input_author, self.input_pages)
+        self.temp_button = Button(text=("{:<40s} by {:<20} {}pages".format(self.input_title, self.input_author, self.input_pages)))
+        self.root.ids.entriesBox.add_widget(self.temp_button)
         self.root.ids.input_title.text = ""
         self.root.ids.input_author.text = ""
         self.root.ids.input_pages.text = ""
@@ -66,13 +79,11 @@ class ReadingListApp(App):
     def press(self, instance):
         pass
 
+    def remove_stuff(self):
+        self.remove_widget(self)
+
     def clear_all(self):
         self.root.ids.entriesBox.clear_widgets()
-
-    def build(self):
-        self.title = "Reading list application"
-        self.root = Builder.load_file("app.kv")
-        return self.root
 
 
 ReadingListApp().run()
